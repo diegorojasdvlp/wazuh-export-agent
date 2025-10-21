3295a7042515544659285863b91d9c05f1f28d65
Diego Rojas <diego.rojasbu@usach.cl>
2025-10-20 23:09:44 -0300

    First Commit

---
+++ src/main/java/cl/usach/glpi/agent/librenms/UsachGlpiLibrenmsAgentApplication.java
@@ -0,0 +1,13 @@
+package cl.usach.glpi.agent.librenms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsachGlpiLibrenmsAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsachGlpiLibrenmsAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/usach/glpi/agent/librenms/UsachGlpiLibrenmsAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.usach.glpi.agent.librenms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsachGlpiLibrenmsAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
