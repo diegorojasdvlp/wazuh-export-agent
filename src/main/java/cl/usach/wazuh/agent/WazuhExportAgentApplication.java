5c28f17e692c4f79097a24bda257668719590298
Diego Rojas <diego.rojasbu@usach.cl>
2026-01-05 13:16:54 -0300

    Quality gate failed

--- src/main/java/cl/usach/wazuh/agent/WazuhExportAgentApplication.java
+++ src/main/java/cl/usach/wazuh/agent/WazuhExportAgentApplication.java
@@ -1,10 +1,7 @@
 package cl.usach.wazuh.agent;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Scanner;
 
 @SpringBootApplication
 public class WazuhExportAgentApplication {
