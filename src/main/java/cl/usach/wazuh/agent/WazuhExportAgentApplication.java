a8796dc3d57965193e524b649eca5347af9f8585
Diego Rojas <diego.rojasbu@usach.cl>
2026-03-11 12:24:10 -0300

    Agrega run for pentesting

--- src/main/java/cl/usach/wazuh/agent/WazuhExportAgentApplication.java
+++ src/main/java/cl/usach/wazuh/agent/WazuhExportAgentApplication.java
@@ -1,6 +1,5 @@
 package cl.usach.wazuh.agent;
 
-
 import cl.usach.wazuh.agent.service.VulnerabilityStatusService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -12,6 +11,7 @@ public class WazuhExportAgentApplication implements CommandLineRunner {
 
 	@Autowired
 	private VulnerabilityStatusService service;
+
 	public static void main(String[] args) {
 		SpringApplication.run(WazuhExportAgentApplication.class, args);
 	}
@@ -19,10 +19,18 @@ public class WazuhExportAgentApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) {
 
-		service.syncVulnerabilityStatus().doOnNext(status -> System.out.println("Saved status: " + status))
-				.doOnError(e -> System.out.println("Error: " + e.getMessage()))
-				.doOnComplete(() -> System.out.println("Sync completed"))
-				.subscribe();
+		if (args.length > 0 && args[0].equals("pentest")) {
+			try {
+				Thread.sleep(30000); // Keep alive for 60 seconds
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+		} else {
+			service.syncVulnerabilityStatus().doOnNext(status -> System.out.println("Saved status: " + status))
+					.doOnError(e -> System.out.println("Error: " + e.getMessage()))
+					.doOnComplete(() -> System.out.println("Sync completed"))
+					.subscribe();
+		}
 		System.exit(0);
 	}
 }
