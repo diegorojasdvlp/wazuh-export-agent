5fff729264d498212753950e607d59bf78014bce
bohemianrhaps0dy <163688066+bohemianrhaps0dy@users.noreply.github.com>
2026-03-11 17:54:25 -0300

    Overload openTunnel method to accept local port

--- src/main/java/cl/usach/wazuh/agent/config/SshTunnelManager.java
+++ src/main/java/cl/usach/wazuh/agent/config/SshTunnelManager.java
@@ -13,6 +13,10 @@ public class SshTunnelManager {
     private final Map<String, Session> sessions = new HashMap<>();
 
     public int openTunnel(String tunnelKey, WazuhProperties props, int remotePort) throws Exception {
+        return this.openTunnel(0,tunnelKey,props,remotePort);
+    }
+
+    public int openTunnel(int lport, String tunnelKey, WazuhProperties props, int remotePort) throws Exception {
         JSch jsch = new JSch();
 
         Session session = jsch.getSession(props.getSshUser(), props.getSshHost(), props.getSshPort());
@@ -20,7 +24,7 @@ public class SshTunnelManager {
         session.setConfig("StrictHostKeyChecking", "no");
         session.connect(10_000);
 
-        int localPort = session.setPortForwardingL(0, "localhost", remotePort);
+        int localPort = session.setPortForwardingL(lport, "localhost", remotePort);
         sessions.put(tunnelKey, session);
 
         System.out.println("SSH tunnel opened [" + tunnelKey + "] localhost:" + localPort + " -> " + props.getSshHost() + ":" + remotePort);
