9cec4c3cb2b8ea0458329fec71b2113d951bb81b
Diego Rojas <diego.rojasbu@usach.cl>
2026-03-12 12:28:39 -0300

    Adding logs

--- src/main/java/cl/usach/wazuh/agent/config/SshTunnelManager.java
+++ src/main/java/cl/usach/wazuh/agent/config/SshTunnelManager.java
@@ -13,7 +13,7 @@ public class SshTunnelManager {
     private final Map<String, Session> sessions = new HashMap<>();
 
     public int openTunnel(String tunnelKey, WazuhProperties props, int remotePort) throws Exception {
-        return this.openTunnel(0,tunnelKey,props,remotePort);
+        return this.openTunnel(0, tunnelKey, props, remotePort);
     }
 
     public int openTunnel(int lport, String tunnelKey, WazuhProperties props, int remotePort) throws Exception {
@@ -27,7 +27,8 @@ public class SshTunnelManager {
         int localPort = session.setPortForwardingL(lport, "localhost", remotePort);
         sessions.put(tunnelKey, session);
 
-        System.out.println("SSH tunnel opened [" + tunnelKey + "] localhost:" + localPort + " -> " + props.getSshHost() + ":" + remotePort);
+        System.out.println("SSH tunnel opened [" + tunnelKey + "] localhost:" + localPort + " -> " + props.getSshHost()
+                + ":" + remotePort + " (via SSH " + props.getSshHost() + ":" + props.getSshPort() + ")");
         return localPort;
     }
 
